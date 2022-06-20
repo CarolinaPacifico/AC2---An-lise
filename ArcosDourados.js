057bb8bbb66e95d59d67942cc6d920b163a9489c
CarolinaPacifico <84384263+CarolinaPacifico@users.noreply.github.com>
2022-06-20 10:05:05 -0300

    Add files via upload

--- ArcosDourados.js
+++ ArcosDourados.js
@@ -13,27 +13,27 @@ class lanche{
     vender(McLancheAlegre, BigLanche, ComboBigLanche, McHen, ComboMcHen, vendas){
 
     if(McLancheAlegre === true){
-        vendas = vendas + 1
+        this.vendas = this.vendas + 1
         const McLancheAlegre = "McLanche Alegre + batata pequena + fruta"
     }
 
     else if(BigLanche === true){
-        vendas = vendas + 1
+        this.vendas = this.vendas + 1
         const BigLanche = "BigLanche (dois hambúrgueres, alface, queijo, molho bacana, cebola, picles e pão)"
     }
 
     else if(ComboBigLanche === true){
-        vendas = vendas + 1
+        this.vendas = this.vendas + 1
         const ComboBigLanche = "BigLanche + batata média + bebida"
     }
 
     else if(McHen === true){
-        vendas = vendas + 1
+        this.vendas = this.vendas + 1
         const McHen = "McHen (hambúrguer de frango, alface, queijo e picles)"
     }
 
     else if(ComboMcHen === true){
-        vendas = vendas + 1
+        this.vendas = this.vendas + 1
         const ComboMcHen = "McHen + batata média + bebida"
     }
 
